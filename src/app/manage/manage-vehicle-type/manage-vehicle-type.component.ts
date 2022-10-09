160a3c2a82e43181aec7575a1404d5a5d5cda6f1
Mario Nocnik <mario.nocnik@xaidat.com>
2022-10-09 21:30:16 +0200

    Drawing of Response works

--- src/app/manage/manage-vehicle-type/manage-vehicle-type.component.ts
+++ src/app/manage/manage-vehicle-type/manage-vehicle-type.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from "rxjs";
 
-import {ManageVehicleTypeService} from "./manage-vehicle-type.service";
+import {ManageVehicleTypeService, VehicleResponse} from "./manage-vehicle-type.service";
 
 @Component({
   selector: 'app-manage-vehicle-type',
@@ -10,6 +10,7 @@ import {ManageVehicleTypeService} from "./manage-vehicle-type.service";
 })
 export class ManageVehicleTypeComponent implements OnInit {
   title: string = "WORKS: ManageVehicleType";
+  vehicleResponse: VehicleResponse | undefined;
 
   constructor(private manageVehicleTypeService: ManageVehicleTypeService) {
   }
@@ -18,8 +19,9 @@ export class ManageVehicleTypeComponent implements OnInit {
     let body = {query: "{ vehicle(uuid: \"b194aaee-089d-4bf0-9feb-311d2c0f62f8\") { uuid name description image vehicleType { name description image } data { value property {  name  unitShort  sortIndex } } }}"};
     let observable: Observable<any> = this.manageVehicleTypeService.preparePost(body);
     observable.subscribe(
-      (data: any) => {
-        console.log("Success", data.data);
+      (response) => {
+        this.vehicleResponse = response.data.vehicle as VehicleResponse;
+        console.log("Success", this.vehicleResponse);
       },
       error => {
         console.log("Error", error);
--- src/app/manage/manage-vehicle-type/manage-vehicle-type.service.ts
+++ src/app/manage/manage-vehicle-type/manage-vehicle-type.service.ts
@@ -2,6 +2,45 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 
+
+export type VehicleDataResponse = {
+  created: string
+  uuid: string
+  value: string
+  property: VehiclePropertyResponse
+  vehicle: VehicleResponse
+}
+
+export type VehiclePropertyResponse = {
+  created: string
+  uuid: string
+  name: string
+  unitShort: string
+  sortIndex: number
+  vehicleType: VehicleTypeResponse
+  data: VehicleDataResponse[]
+}
+
+export type VehicleTypeResponse = {
+  created: string;
+  uuid: string;
+  name: string
+  description: string
+  image: string
+  vehicles: VehicleResponse[]
+  properties: VehiclePropertyResponse[]
+}
+
+export type VehicleResponse = {
+  created: string;
+  uuid: string;
+  name: string
+  image: string
+  description: string
+  vehicleType: VehicleTypeResponse
+  data: VehicleDataResponse[]
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,7 +57,7 @@ export class ManageVehicleTypeService {
   }
 
   preparePost(body: object) {
-    return this.http.post(
+    return this.http.post<VehicleResponse>(
       environment.graphEndpoint,
       JSON.stringify(body),
       {headers: this.headers}
